f1c2face70710d93c976b1d7420011ba00a7a08b
teja justin <mandysumo184@gmail.com>
2023-07-27 15:21:22 +0530

    tejajustin commit

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import {
+	ThirdwebProvider,
+	ConnectWallet,
+	metamaskWallet,
+} from "@thirdweb-dev/react";
+import { StateContextProvider } from "./context";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Sepolia } from "@thirdweb-dev/chains";
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+	<ThirdwebProvider activeChain={Sepolia} supportedWallets={[metamaskWallet()]}>
+		<Router>
+			<StateContextProvider>
+				<App />
+			</StateContextProvider>
+		</Router>
+	</ThirdwebProvider>
+);
